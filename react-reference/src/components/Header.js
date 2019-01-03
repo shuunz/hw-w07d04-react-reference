fe61f5956cea48456983a4cc1ef78cbadc9f0fbc
shuunz <shuunz120@gmail.com>
2019-01-03 08:48:38 +0300

    hw-w07d04-react-reference

---
+++ react-reference/src/components/Header.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+
+class Header extends Component{
+render(){
+    return(
+        <div>
+            <h1 className="App-header">React is Awesome!</h1>
+            <image></image>
+        </div>
+
+    )
+    }
+}
+
+
+
+export default Header;
